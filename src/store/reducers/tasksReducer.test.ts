da55b61ab563f4f6c07baeb9f402b5c0e3d2c342
NikChist88 <NikChistyakov88@yandex.ru>
2023-12-19 11:15:02 +0300

    add tasks reducer and test

---
+++ src/store/reducers/tasksReducer.test.ts
@@ -0,0 +1,93 @@
+import { TasksType } from '../store'
+import { TaskType } from '../store'
+import { tasksReducer } from './tasksReducer'
+import { RemoveTaskAC } from './tasksReducer'
+import { AddTaskAC } from './tasksReducer'
+import { UpdateTaskTitleAC } from './tasksReducer'
+import { ChangeTaskStatusAC } from './tasksReducer'
+import { v1 } from 'uuid'
+
+// Delete Task
+test('remove task from todolist', () => {
+  const todolistId_1: string = v1()
+
+  const startState: TasksType = {
+    [todolistId_1]: [
+      { id: v1(), title: 'html', isDone: false },
+      { id: v1(), title: 'css', isDone: false },
+      { id: v1(), title: 'js', isDone: false },
+    ],
+  }
+
+  const taskId = startState[todolistId_1][0].id
+
+  const action = RemoveTaskAC(taskId, todolistId_1)
+
+  const endState = tasksReducer(startState, action)
+
+  expect(endState[todolistId_1].length).toBe(2)
+})
+
+// Add Task
+test('add task for todolist', () => {
+  const todolistId_1: string = v1()
+
+  const startState: TasksType = {
+    [todolistId_1]: [
+      { id: v1(), title: 'html', isDone: false },
+      { id: v1(), title: 'css', isDone: false },
+      { id: v1(), title: 'js', isDone: false },
+    ],
+  }
+
+  const newTask: TaskType = { id: v1(), title: 'react', isDone: false }
+
+  const action = AddTaskAC(newTask, todolistId_1)
+
+  const endState = tasksReducer(startState, action)
+
+  expect(endState[todolistId_1].length).toBe(4)
+})
+
+// Update Task Title
+test('update task title', () => {
+  const todolistId_1: string = v1()
+
+  const startState: TasksType = {
+    [todolistId_1]: [
+      { id: v1(), title: 'html', isDone: false },
+      { id: v1(), title: 'css', isDone: false },
+      { id: v1(), title: 'js', isDone: false },
+    ],
+  }
+
+  const taskId = startState[todolistId_1][0].id
+  const newTaskTitle: string = 'svelte'
+
+  const action = UpdateTaskTitleAC(newTaskTitle, taskId, todolistId_1)
+
+  const endState = tasksReducer(startState, action)
+
+  expect(endState[todolistId_1][0].title).toBe('svelte')
+})
+
+// Change Task Status
+test('change task status', () => {
+  const todolistId_1: string = v1()
+
+  const startState: TasksType = {
+    [todolistId_1]: [
+      { id: v1(), title: 'html', isDone: false },
+      { id: v1(), title: 'css', isDone: false },
+      { id: v1(), title: 'js', isDone: false },
+    ],
+  }
+
+  const taskId = startState[todolistId_1][0].id
+
+  const action = ChangeTaskStatusAC(taskId, todolistId_1)
+
+  const endState = tasksReducer(startState, action)
+
+  expect(endState[todolistId_1][0].isDone).toBe(true)
+})
---
+++ src/store/reducers/tasksReducer.ts
@@ -0,0 +1,104 @@
+import { TasksType } from '../store'
+import { TaskType } from '../store'
+
+type RemoveTaskActionType = {
+  type: 'REMOVE-TASK'
+  id: string
+  todolistId: string
+}
+
+type AddTaskActionType = {
+  type: 'ADD-TASK'
+  newTask: TaskType
+  todolistId: string
+}
+
+type UpdateTaskTitleActionType = {
+  type: 'UPDATE-TASK-TITLE'
+  newTaskTitle: string
+  taskId: string
+  todolistId: string
+}
+
+type ChangeTaskStatusActionType = {
+  type: 'CHANGE-TASK-STATUS'
+  taskId: string
+  todolistId: string
+}
+
+type ActionsType =
+  | RemoveTaskActionType
+  | AddTaskActionType
+  | UpdateTaskTitleActionType
+  | ChangeTaskStatusActionType
+
+export const RemoveTaskAC = (
+  id: string,
+  todolistId: string
+): RemoveTaskActionType => {
+  return { type: 'REMOVE-TASK', id: id, todolistId: todolistId }
+}
+
+export const AddTaskAC = (
+  newTask: TaskType,
+  todolistId: string
+): AddTaskActionType => {
+  return { type: 'ADD-TASK', newTask: newTask, todolistId: todolistId }
+}
+
+export const UpdateTaskTitleAC = (
+  newTaskTitle: string,
+  taskId: string,
+  todolistId: string
+): UpdateTaskTitleActionType => {
+  return {
+    type: 'UPDATE-TASK-TITLE',
+    newTaskTitle: newTaskTitle,
+    taskId: taskId,
+    todolistId: todolistId,
+  }
+}
+
+export const ChangeTaskStatusAC = (
+  taskId: string,
+  todolistId: string
+): ChangeTaskStatusActionType => {
+  return { type: 'CHANGE-TASK-STATUS', taskId: taskId, todolistId: todolistId }
+}
+
+export const tasksReducer = (
+  state: TasksType,
+  action: ActionsType
+): TasksType => {
+  switch (action.type) {
+    case 'REMOVE-TASK': {
+      const updatedTasks = state[action.todolistId].filter(
+        (t) => t.id !== action.id
+      )
+      return { ...state, [action.todolistId]: updatedTasks }
+    }
+
+    case 'ADD-TASK':
+      return {
+        ...state,
+        [action.todolistId]: [action.newTask, ...state[action.todolistId]],
+      }
+
+    case 'UPDATE-TASK-TITLE': {
+      const updatedTasks = state[action.todolistId].map((t) =>
+        t.id === action.taskId ? { ...t, title: action.newTaskTitle } : t
+      )
+      return { ...state, [action.todolistId]: updatedTasks }
+    }
+
+    case 'CHANGE-TASK-STATUS': {
+      const updatedTasks = state[action.todolistId].map((t) =>
+        t.id === action.taskId ? { ...t, isDone: true } : t
+      )
+      return { ...state, [action.todolistId]: updatedTasks }
+    }
+
+    default:
+      throw new Error('Wrong action type!')
+  }
+}
